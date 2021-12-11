bf5328ab7beb8c5db42ced160f5d62b0021e7488
AlekseyBabushkin <babushkin058@mail.ru>
2021-12-11 22:58:56 +0300

    add services

---
+++ src/js/components/accordions.js
@@ -0,0 +1,10 @@
+if(document.querySelector('.we-offer')) {
+	const accordion1 = new GraphAccordion('.accordion-1', {
+		speed: 400
+	});
+
+	const accordion2 = new GraphAccordion('.accordion-2', {
+		speed: 400
+	});
+}
+
